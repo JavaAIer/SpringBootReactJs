66b4c7b15eb66856d561339262083298462b917e
cuteui@qq.com <cuteui@qq.com>
2019-06-13 14:56:07 +0800

    2019-6-13

---
+++ src/main/java/com/javaaier/sbrj/SbrjApplication.java
@@ -0,0 +1,13 @@
+package com.javaaier.sbrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbrjApplication.class, args);
+    }
+
+}
